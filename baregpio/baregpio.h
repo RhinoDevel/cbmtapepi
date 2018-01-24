edc4ab9c5e928f4614417d6682e7fb41d767c8e4
Marc <RhinoDevel@users.noreply.github.com>
2018-01-24 18:18:15 +0100

    Prevent multiple header inclusions for bare GPIO.

--- baregpio/baregpio.h
+++ baregpio/baregpio.h
@@ -1,8 +1,13 @@
 
 // Marcel Timm, RhinoDevel, 2018jan22
 
+#ifndef MT_BAREGPIO
+#define MT_BAREGPIO
+
 #include <stdbool.h>
 #include <stdint.h>
 
 void baregpio_write(uint32_t const pin_nr, bool const high);
 void baregpio_set_output(uint32_t const pin_nr, bool const high);
+
+#endif //MT_BAREGPIO
