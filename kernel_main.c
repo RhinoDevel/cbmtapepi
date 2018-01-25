57f46caaf796e5896ecf51fa05e6c341610649d1
Marc <RhinoDevel@users.noreply.github.com>
2018-01-25 16:58:13 +0100

    Move auxiliary peripherals stuff to own header file.

--- kernel_main.c
+++ kernel_main.c
@@ -14,32 +14,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "peribase.h"
+#include "auxiliary.h"
 #include "baregpio/baregpio.h"
 #include "mem/mem.h"
 #include "busywait/busywait.h"
-
-// Auxiliary peripherals register map (see page 8):
-//
-#define AUX_BASE (PERI_BASE + 0x215000)
-#define AUX_IRQ AUX_BASE // Auxiliary interrupt status (AUXIRQ).
-#define AUX_ENABLES (AUX_BASE + 0x04) // Auxiliary enables (AUXENB).
-#define AUX_MU_IO_REG (AUX_BASE + 0x40) // Mini UART I/O data.
-#define AUX_MU_IER_REG (AUX_BASE + 0x44) // Mini UART interrupt enable.
-#define AUX_MU_IIR_REG (AUX_BASE + 0x48) // Mini UART interrupt identify.
-#define AUX_MU_LCR_REG (AUX_BASE + 0x4C) // Mini UART line control.
-#define AUX_MU_MCR_REG (AUX_BASE + 0x50) // Mini UART modem control.
-#define AUX_MU_LSR_REG (AUX_BASE + 0x54) // Mini UART line status.
-#define AUX_MU_MSR_REG (AUX_BASE + 0x58) // Mini UART modem status.
-#define AUX_MU_SCRATCH (AUX_BASE + 0x5C) // Mini UART scratch.
-#define AUX_MU_CNTL_REG (AUX_BASE + 0x60) // Mini UART extra control.
-#define AUX_MU_STAT_REG (AUX_BASE + 0x64) // Mini UART extra status.
-#define AUX_MU_BAUD_REG (AUX_BASE + 0x68) // Mini UART Baud rate.
-//
-// The Mini UART emulates 16550 behaviour, but is NOT a 16650 compatible UART.
-//
-// In addition to the Mini UART there also exist two SPI masters
-// as auxiliary peripherals.
 
 extern uint32_t __heap; // There is not really an uint32_t object allocated.
 
