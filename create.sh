d5da32f186f2e5daf70e9ae4b3f89e37cbdf1627
Marc <RhinoDevel@users.noreply.github.com>
2018-10-19 16:44:06 +0200

    Add/copied instructions to build cross compiler.

--- create.sh
+++ create.sh
@@ -2,6 +2,36 @@
 
 # Create a cross compiler as described at:
 # http://wiki.osdev.org/GCC_Cross-Compiler
+#
+# E.g. like this:
+# ---------------
+#
+# wget https://mirror.checkdomain.de/gnu/binutils/binutils-2.31.1.tar.bz2
+# wget https://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.xz
+#
+#
+#export PREFIX="$HOME/Documents/pios/cross/result"
+#export TARGET=arm-none-eabi
+#export PATH="$PREFIX/bin:$PATH"
+#
+#cd $HOME/Documents/pios/cross
+#tar xvjf binutils-2.31.1.tar.bz2
+#mkdir build-binutils
+#cd build-binutils
+#../binutils-2.31.1/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+#make
+#make install
+#
+#cd $HOME/Documents/pios/cross
+#
+#tar xvf gcc-8.2.0.tar.xz
+#mkdir build-gcc
+#cd build-gcc
+#../gcc-8.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers
+#make all-gcc
+#make all-target-libgcc
+#make install-gcc
+#make install-target-libgcc
 
 # Get bootcode.bin, start.elf and fixup.dat from:
 # https://github.com/raspberrypi/firmware/tree/master/boot
