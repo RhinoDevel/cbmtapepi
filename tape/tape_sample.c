d40e87154c1426a0e60f80867bedddcf8d2bc515
Marc <RhinoDevel@users.noreply.github.com>
2018-11-01 11:59:42 +0100

    Switch to C64 code being enabled; Comment about file to byte array via xxd command.

--- tape/tape_sample.c
+++ tape/tape_sample.c
@@ -5,6 +5,10 @@
 #include "tape_filetype.h"
 #include "tape_sample.h"
 
+// How to get byte array from (PRG) file:
+//
+// xxd -i 'pet graphics.prg' > pet_graphics.c
+
 void tape_sample_fill_buf(struct tape_input * const buf)
 {
     static uint8_t const bytes[] = {
@@ -12,11 +16,11 @@ void tape_sample_fill_buf(struct tape_input * const buf)
         83, // Heart symbol (yes, it is romantic).
         141, // Absolute STA.
 
-        //0, // Lower byte of 1024 (0x0400 - C64 video RAM start).
-        //4, // Higher byte of 1024.
+        0, // Lower byte of 1024 (0x0400 - C64 video RAM start).
+        4, // Higher byte of 1024.
         //
-        0, // Lower byte of 32768 (0x8000 - PET video RAM start).
-        128, // Higher byte of 32768.
+        //0, // Lower byte of 32768 (0x8000 - PET video RAM start).
+        //128, // Higher byte of 32768.
 
         96 // RTS.
     };
@@ -34,11 +38,11 @@ void tape_sample_fill_buf(struct tape_input * const buf)
         }
     }
 
-    //buf->type = tape_filetype_non_relocatable; // C64
-    buf->type = tape_filetype_relocatable; // PET (for machine language, too).
+    buf->type = tape_filetype_non_relocatable; // C64
+    //buf->type = tape_filetype_relocatable; // PET (for machine language, too).
 
-    //buf->addr = 2024; // C64: 16 unused bytes.
-    buf->addr = 826; // PET ROM v2 and v3 tape #2 buffer.
+    buf->addr = 2024; // C64: 16 unused bytes.
+    //buf->addr = 0x0401;//826; // PET ROM v2 and v3 tape #2 buffer.
 
     buf->bytes = bytes;
     buf->len = sizeof bytes;
