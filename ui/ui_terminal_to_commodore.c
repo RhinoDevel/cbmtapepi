ff225ec5111338784021cb32ae2a672a3340cbed
Marc <RhinoDevel@users.noreply.github.com>
2019-01-06 08:36:23 +0100

    Use YMODEM instead of XMODEM; No receive loop.

--- ui/ui_terminal_to_commodore.c
+++ ui/ui_terminal_to_commodore.c
@@ -10,8 +10,9 @@
 #include "../config.h"
 #include "../console/console.h"
 #include "../alloc/alloc.h"
-#include "../xmodem/xmodem.h"
-#include "../xmodem/xmodem_receive_params.h"
+#include "../ymodem/ymodem.h"
+#include "../ymodem/ymodem_receive_params.h"
+#include "../ymodem/ymodem_receive_err.h"
 #include "../miniuart/miniuart.h"
 #include "../tape/tape_send.h"
 #include "../tape/tape_input.h"
@@ -94,25 +95,38 @@ void ui_terminal_to_commodore()
 {
     static uint32_t const len = 64 * 1024; // 64 kB.
 
-    struct xmodem_receive_params p;
-    uint32_t count = 0;
+    struct ymodem_receive_params p;
+    enum ymodem_receive_err e;
 
     p.write_byte = miniuart_write_byte;
     p.read_byte = miniuart_read_byte;
     p.is_ready_to_read = miniuart_is_ready_to_read;
     p.buf = alloc_alloc(len * sizeof (uint8_t));
-    p.len = len;
+    p.buf_len = len;
+    p.file_len = 0;
+    p.name[0] = '\0';
 
-    console_writeline("Please send your file, now (via XMODEM).");
-    while(!xmodem_receive(&p, &count))
+    console_writeline("Please send your file, now (via YMODEM).");
+
+    e = ymodem_receive(&p);
+    if(e != ymodem_receive_err_none)
     {
-        console_writeline("Failed to receive file! Retrying..");
+        console_write("Failed to receive file (error ");
+        console_write_dword_dec((uint32_t)e);
+        console_writeline(")!");
+
+        alloc_free(p.buf);
+        p.buf = 0;
+        return;
     }
-    console_write("Received ");
-    console_write_dword_dec(count);
+
+    console_write("Received file \"");
+    console_write(p.name);
+    console_write("\" with a length of ");
+    console_write_dword_dec(p.file_len);
     console_writeline(" bytes.");
 
-    send_to_commodore(p.buf, count);
+    send_to_commodore(p.buf, p.file_len);
 
     alloc_free(p.buf);
     p.buf = 0;
