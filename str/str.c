21c1f5f607c745be2c8deb93fac10fc0b948c0c4
Marc <RhinoDevel@users.noreply.github.com>
2019-01-07 07:51:30 +0100

    str_to_lower() and str_to_upper().

--- str/str.c
+++ str/str.c
@@ -2,8 +2,38 @@
 // Marcel Timm, RhinoDevel, 2019jan05
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "str.h"
+
+static bool is_upper_case_letter(char const c)
+{
+    return c >= 'A' && c <= 'Z';
+}
+static bool is_lower_case_letter(char const c)
+{
+    return c >= 'a' && c <= 'z';
+}
+static char get_upper_case(char const c)
+{
+    static char const diff = 'a' - 'A';
+
+    if(is_lower_case_letter(c))
+    {
+        return c - diff;
+    }
+    return '?';
+}
+static char get_lower_case(char const c)
+{
+    static char const diff = 'a' - 'A';
+
+    if(is_upper_case_letter(c))
+    {
+        return c + diff;
+    }
+    return '?';
+}
 
 uint32_t str_get_len(char const * const s)
 {
@@ -14,4 +44,36 @@ uint32_t str_get_len(char const * const s)
         ++ret_val;
     }
     return ret_val;
+}
+
+void str_to_upper(char * const s_out, char const * const s_in)
+{
+    int i = 0;
+
+    for(;s_in[i] != '\0';++i)
+    {
+        if(is_lower_case_letter(s_in[i]))
+        {
+            s_out[i] = get_upper_case(s_in[i]);
+            continue;
+        }
+        s_out[i] = s_in[i];
+    }
+    s_out[i] = '\0';//s_in[i];
+}
+
+void str_to_lower(char * const s_out, char const * const s_in)
+{
+    int i = 0;
+
+    for(;s_in[i] != '\0';++i)
+    {
+        if(is_upper_case_letter(s_in[i]))
+        {
+            s_out[i] = get_lower_case(s_in[i]);
+            continue;
+        }
+        s_out[i] = s_in[i];
+    }
+    s_out[i] = '\0';//s_in[i];
 }
--- str/str.h
+++ str/str.h
@@ -8,4 +8,14 @@
 
 uint32_t str_get_len(char const * const s);
 
+/**
+ * - In and output strings may be equal.
+ */
+void str_to_upper(char * const s_out, char const * const s_in);
+
+/**
+ * - In and output strings may be equal.
+ */
+void str_to_lower(char * const s_out, char const * const s_in);
+
 #endif //MT_STR
