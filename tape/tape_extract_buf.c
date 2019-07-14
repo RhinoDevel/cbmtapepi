d6b5f3d84936cad4ed7ceecf2987891bcb3a1ffb
Marc <RhinoDevel@users.noreply.github.com>
2019-07-14 12:14:13 +0200

    extract_data_following_sync() skeleton.

--- tape/tape_extract_buf.c
+++ tape/tape_extract_buf.c
@@ -22,29 +22,49 @@ static void consume_sync(uint8_t const * const buf, int * const pos)
     }
 }
 
-// static void extract_data_following_sync(
-//     uint8_t * const data,
-//     uint32_t const len,
-//     uint8_t const * const buf,
-//     int * const pos)
-// {
-//     // 1st data transmit:
-//
-//     add_data_transmit(false, data, len, buf, pos);
-//
-//     // 2nd data transmit:
-//
-//     add_data_transmit(true, data, len, buf, pos);
-// }
+/**
+ * - Caller takes ownership of return value.
+ */
+static uint8_t* extract_data_following_sync(
+    uint8_t const * const buf, int * const pos, int const * len)
+{
+    // TODO: Implement!
+    //
+    (void)buf;
+    (void)pos;
+    (void)len;
+    return 0;
+    //
+    // // 1st data transmit:
+    //
+    // add_data_transmit(false, data, len, buf, pos);
+    //
+    // // 2nd data transmit:
+    //
+    // add_data_transmit(true, data, len, buf, pos);
+}
 
 static bool extract_headerdatablock(
     uint8_t const * const buf, int * const pos, struct tape_input * const input)
 {
     (void)input; // TODO: Remove!
 
+    uint8_t* data;
+    int len;
+
     // Synchronization:
 
     consume_sync(buf, pos);
+
+    // Payload:
+
+    data = extract_data_following_sync(buf, pos, &len);
+    if(data == 0)
+    {
+        return false;
+    }
+
+    alloc_free(data);
 
     // TODO: Implement!
     //
