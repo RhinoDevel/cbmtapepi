cd0328634947bfcaa02c0a1563cf6ccd509caf9d
Marc <RhinoDevel@users.noreply.github.com>
2019-07-28 08:36:12 +0200

    Move GPIO setup to top of MiniUART init function; Improve comments.

--- miniuart/miniuart.c
+++ miniuart/miniuart.c
@@ -53,17 +53,10 @@ void miniuart_write_byte(uint8_t const byte)
 
 void miniuart_init()
 {
-    mem_write(
-        AUX_ENABLES, 1); // Enables Mini UART (disables SPI1 & SPI2, page 9).
-    mem_write(AUX_MU_IER_REG, 0); // See page 12 (yes, page 12!).
-    mem_write(AUX_MU_CNTL_REG, 0); // Not using extra features. See page 16.
-    mem_write(AUX_MU_LCR_REG, 3); // Sets 8 bit mode (error on page 14!).
-    mem_write(AUX_MU_MCR_REG, 0); // Sets UART1_RTS line to high (page 14).
-    mem_write(AUX_MU_IER_REG, 0); // See page 12 (yes, page 12!).
-    mem_write( // Clears receive and transmit FIFOs (page 13, yes, page 13!).
-        AUX_MU_IIR_REG, 0xC6/*11000110*/);
-    mem_write( // Hard-coded for 250 MHz [((250,000,000/115200)/8)-1 = 270]!
-        AUX_MU_BAUD_REG, 270);
+    // Enable pull-down resistors (page 101):
+    //
+    baregpio_set_pud(14, gpio_pud_down);
+    baregpio_set_pud(15, gpio_pud_down);
 
     // Set GPIO pin 14 and 15 to alternate function 5 (page 92 and page 102),
     // which is using UART1:
@@ -71,10 +64,48 @@ void miniuart_init()
     baregpio_set_func(14, gpio_func_alt5); // TXD1
     baregpio_set_func(15, gpio_func_alt5); // RXD1
 
-    // Enable pull-down resistors (page 101):
+    // Enable Mini UART (disables SPI1 & SPI2, page 9):
     //
-    baregpio_set_pud(14, gpio_pud_down);
-    baregpio_set_pud(15, gpio_pud_down);
+    mem_write(AUX_ENABLES, 1/*...001*/);
 
-    mem_write(AUX_MU_CNTL_REG, 3);
+    // Disable receive and transmit interrupts (see page 12 - yes, page 12!):
+    //
+    mem_write(AUX_MU_IER_REG, 0);
+
+    // Disable auto flow control (see page 16):
+    //
+    mem_write(AUX_MU_CNTL_REG, 0);
+
+    // Set 8 bit mode (instead of 7 bit mode; error on page 14!):
+    //
+    mem_write(AUX_MU_LCR_REG, 3/*...00XXXX11*/);
+
+    mem_write(AUX_MU_MCR_REG, 0); // Sets UART1_RTS line to high (page 14).
+
+    // AGAIN (maybe not necessary to do this twice?):
+    //
+    // Disable receive and transmit interrupts (see page 12 - yes, page 12!):
+    //
+    mem_write(AUX_MU_IER_REG, 0);
+
+    // Clear receive and transmit FIFOs (page 13, yes, page 13!):
+    //
+    mem_write(AUX_MU_IIR_REG, 0xC6/*...11000110*/);
+
+    // Hard-coded for 250 MHz [((250,000,000/115200)/8)-1 = 270]:
+    //
+    mem_write(AUX_MU_BAUD_REG, 270);
+
+    // Enables receiver and transmitter (see page 17):
+    //
+    mem_write(AUX_MU_CNTL_REG, 3/*...00000011*/);
+
+    // Notes:
+    // ------
+    //
+    // Flow control via extra GPIO PINs and cables (not used, here):
+    // - RTS = Request to send.
+    // - CTS = Clear to send.
+    //
+    // There is also auto-flow control (disable, here).
 }
