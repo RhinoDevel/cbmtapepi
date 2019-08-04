356e9e647297e361f256858a3dee3e2c9b05e405
Marc <RhinoDevel@users.noreply.github.com>
2019-08-04 18:00:41 +0200

    Improve output to user.

--- tape/tape_init.c
+++ tape/tape_init.c
@@ -11,13 +11,13 @@
 
 void tape_init()
 {
-    console_writeline("tape_init: Setting sense line to HIGH..");
+    console_writeline("tape_init: Setting sense output line to HIGH..");
     baregpio_set_output(MT_TAPE_GPIO_PIN_NR_SENSE, true);
 
     console_writeline("tape_init: Setting motor line to input with pull-down..");
     baregpio_set_input_pull_down(MT_TAPE_GPIO_PIN_NR_MOTOR);
 
-    console_writeline("tape_init: Setting tape read line to HIGH..");
+    console_writeline("tape_init: Setting tape read output line to HIGH..");
     baregpio_set_output(MT_TAPE_GPIO_PIN_NR_READ, true);
 
     console_writeline(
