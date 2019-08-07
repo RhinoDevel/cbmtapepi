00d57d7125b2ef388ea60721465407257e12a9fd
Marc <RhinoDevel@users.noreply.github.com>
2019-08-07 23:08:12 +0200

    Improve video out demo code and add some "font" output.

--- video/video.c
+++ video/video.c
@@ -2,47 +2,63 @@
 // Marcel Timm, RhinoDevel, 2019aug07
 
 #include "video.h"
+#include "../peribase.h"
 #include "../mem/mem.h"
+#include "../ascii/ascii.h"
 
 #include <stdint.h>
 #include <stdbool.h>
 
-static uint32_t const s_mailbox_addr = 0x2000B880;
+static uint32_t const s_mailbox_base = PERI_BASE + 0xB880; // Mailbox 0.
 
-static uint32_t mailbox_write(
-    uint32_t const fbinfo_addr, uint32_t const channel)
+static uint32_t const s_mailbox_read = s_mailbox_base;
+//
+// Upper 28 bits hold value, lower 4 bits hold the sending channel's number.
+
+static uint32_t const s_mailbox_status = s_mailbox_base + 0x18;
+
+static uint32_t const s_mailbox_write = s_mailbox_base + 0x20;
+//
+// Upper 28 bits hold value, lower 4 bits hold the receiving channel's number.
+
+static uint32_t const s_status_empty = 0x40000000;
+static uint32_t const s_status_full = 0x80000000;
+
+/**
+ * - Expects 28 bit value.
+ */
+static void mailbox_write(uint32_t const channel, uint32_t const val)
 {
-    while(true)
-    {
-        if((mem_read(s_mailbox_addr + 0x18) & 0x80000000)==0)
-        {
-            break;
-        }
-    }
-    mem_write(s_mailbox_addr + 0x20, fbinfo_addr + channel);
+    //assert(channel >= 0 && channel < 16);
+    //assert((val <= 0x0FFFFFFF);
 
-    return 0;
+    while((mem_read(s_mailbox_status) & s_status_full) != 0)
+    {
+        // Mailbox is full. Wait.
+    }
+    mem_write(s_mailbox_write, (val << 16) | channel);
 }
 
+/**
+ * - Returns higher 28 bits (without the channel nr.).
+ */
 static uint32_t mailbox_read(uint32_t const channel)
 {
-    uint32_t ret_val;
+    //assert(channel >= 0 && channel < 16);
 
     while(true)
     {
-        while(true)
+        uint32_t val;
+
+        while((mem_read(s_mailbox_status) & s_status_empty) != 0)
         {
-            ret_val = mem_read(s_mailbox_addr + 0x18);
-            if((ret_val & 0x40000000) == 0)
-            {
-                break;
-            }
+            // Mailbox is empty. Wait.
         }
 
-        ret_val = mem_read(s_mailbox_addr + 0);
-        if((ret_val & 0xF) == channel)
+        val = mem_read(s_mailbox_read);
+        if((val & 0xF) == channel) // Lower 4 bits.
         {
-            return ret_val;
+            return val >> 4; // Higher 28 bits.
         }
     }
 
@@ -51,62 +67,63 @@ static uint32_t mailbox_read(uint32_t const channel)
 
 void video_init()
 {
-    uint32_t ra, rb, rx, ry;
+    static uint32_t const channel_nr = 1; // Of mailbox 0.
+    static uint32_t const msg_buf_addr = 0x40040000; // Must be 16-byte aligned.
 
-    mem_write(0x40040000, 640); /* #0 Physical Width */
-    mem_write(0x40040004, 480); /* #4 Physical Height */
-    mem_write(0x40040008, 640); /* #8 Virtual Width */
-    mem_write(0x4004000C, 480); /* #12 Virtual Height */
-    mem_write(0x40040010, 0); /* #16 GPU - Pitch */
-    mem_write(0x40040014, 32); /* #20 Bit Depth */
-    mem_write(0x40040018, 0); /* #24 X */
-    mem_write(0x4004001C, 0); /* #28 Y */
-    mem_write(0x40040020, 0); /* #32 GPU - Pointer */
-    mem_write(0x40040024, 0); /* #36 GPU - Size */
+    static uint32_t const physical_width = 320;
+    static uint32_t const physical_height = 200;
+    static uint32_t const fb_width = physical_width;
+    static uint32_t const fb_height = physical_height;
+    static uint32_t const bit_depth = 32;
 
-    mailbox_write(0x40040000, 1);
-    mailbox_read(1);
+    uint32_t rb, rx, ry;
 
-    rb = 0x40040000;
-    for(ra = 0;ra < 10;++ra)
-    {
-        mem_read(rb);
-        rb += 4;
-    }
+    // DEPRECATED mailbox?
+    //
+    // See: https://github.com/raspberrypi/firmware/wiki/Mailbox-framebuffer-interface
+
+    mem_write(msg_buf_addr, physical_width); /* #0 Physical width. */
+    mem_write(msg_buf_addr + 4, physical_height); /* #4 Physical height. */
+    mem_write(msg_buf_addr + 8, fb_width); /* #8 Virtual width. */
+    mem_write(msg_buf_addr + 12, fb_height); /* #12 Virtual height. */
+    mem_write(msg_buf_addr + 16, 0); /* #16 Pitch. */
+    mem_write(msg_buf_addr + 20, bit_depth); /* #20 Bit depth. */
+    mem_write(msg_buf_addr + 24, 0); /* #24 X offset of virtual framebuffer. */
+    mem_write(msg_buf_addr + 28, 0); /* #28 Y offset of virtual framebuffer. */
+    mem_write(msg_buf_addr + 32, 0); /* #32 Framebuffer address. */
+    mem_write(msg_buf_addr + 36, 0); /* #36 Framebuffer size. */
+
+    mailbox_write(channel_nr, msg_buf_addr >> 16);
+    mailbox_read(channel_nr);
 
     rb = mem_read(0x40040020);
-    for(ra = 0;ra < 10000;++ra)
+    for(ry=0;ry<fb_height;ry++)
     {
-        mem_write(rb,~((ra&0xFF)<<0));
-        rb+=4;
-    }
-    for(ra=0;ra<10000;ra++)
-    {
-        mem_write(rb,~((ra&0xFF)<<8));
-        rb+=4;
-    }
-    for(ra=0;ra<10000;ra++)
-    {
-        mem_write(rb,~((ra&0xFF)<<16));
-        rb+=4;
-    }
-    for(ra=0;ra<10000;ra++)
-    {
-        mem_write(rb,~((ra&0xFF)<<24));
-        rb+=4;
-    }
-    rb=mem_read(0x40040020);
-    ra=0;
-    for(ry=0;ry<480;ry++)
-    {
-        for(rx=0;rx<640;rx++)
+        for(rx=0;rx<fb_width;rx++)
         {
-            uint32_t r = (255 * ry) / 480;
-            uint32_t b = (255 * rx) / 640;
-            uint32_t g = 0;
+            mem_write(rb, 0xFF352879);
+            rb += 4;
+        }
+    }
 
-            mem_write(rb, (0xFF | (r << 16) | (g << 8) | b) );
-            rb+=4;
+    for(int i = 0;i < 6;++i)
+    {
+        for(ry = 0;ry < 8; ++ry)
+        {
+            rb = mem_read(0x40040020) + i * 4 * 8 * fb_width + 4 * ry * fb_width;
+
+            for(rx = 0;rx < 8;++rx)
+            {
+                if(ascii[i][ry * 8 + rx] == 1)
+                {
+                    mem_write(rb, 0xFF6C5EB5);
+                }
+                else
+                {
+                    mem_write(rb, 0xFF352879);
+                }
+                rb += 4;
+            }
         }
     }
 }
