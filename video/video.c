aa7cfcc207363e2037b3117ddb1bbb09d2690ec2
Marc <RhinoDevel@users.noreply.github.com>
2019-08-08 10:58:12 +0200

    Renaming and comments.

--- video/video.c
+++ video/video.c
@@ -9,20 +9,20 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-static uint32_t const s_mailbox_base = PERI_BASE + 0xB880; // Mailbox 0.
+static uint32_t const s_mailbox0_base = PERI_BASE + 0xB880;
 
-static uint32_t const s_mailbox_read = s_mailbox_base;
+static uint32_t const s_mailbox0_read = s_mailbox0_base;
 //
 // Upper 28 bits hold value, lower 4 bits hold the sending channel's number.
 
-static uint32_t const s_mailbox_status = s_mailbox_base + 0x18;
+static uint32_t const s_mailbox0_status = s_mailbox0_base + 0x18;
 
-static uint32_t const s_mailbox_write = s_mailbox_base + 0x20;
+static uint32_t const s_mailbox0_write = s_mailbox0_base + 0x20;
 //
 // Upper 28 bits hold value, lower 4 bits hold the receiving channel's number.
 
-static uint32_t const s_status_empty = 0x40000000;
-static uint32_t const s_status_full = 0x80000000;
+static uint32_t const s_status_empty = 0x40000000; // (bit 30 set, if empty)
+static uint32_t const s_status_full = 0x80000000; // (bit 31 set, if full)
 
 /**
  * - Expects 28 bit value.
@@ -32,11 +32,11 @@ static void mailbox_write(uint32_t const channel, uint32_t const val)
     //assert(channel >= 0 && channel < 16);
     //assert((val <= 0x0FFFFFFF);
 
-    while((mem_read(s_mailbox_status) & s_status_full) != 0)
+    while((mem_read(s_mailbox0_status) & s_status_full) != 0)
     {
         // Mailbox is full. Wait.
     }
-    mem_write(s_mailbox_write, (val << 16) | channel);
+    mem_write(s_mailbox0_write, (val << 16) | channel);
 }
 
 /**
@@ -50,12 +50,12 @@ static uint32_t mailbox_read(uint32_t const channel)
     {
         uint32_t val;
 
-        while((mem_read(s_mailbox_status) & s_status_empty) != 0)
+        while((mem_read(s_mailbox0_status) & s_status_empty) != 0)
         {
             // Mailbox is empty. Wait.
         }
 
-        val = mem_read(s_mailbox_read);
+        val = mem_read(s_mailbox0_read);
         if((val & 0xF) == channel) // Lower 4 bits.
         {
             return val >> 4; // Higher 28 bits.
