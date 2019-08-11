a1e2ce0566256c0c39d13ad32e4abf9aa7d96841
Marc <RhinoDevel@users.noreply.github.com>
2019-08-11 12:56:14 +0200

    Simplify script.

--- serial.sh
+++ serial.sh
@@ -14,8 +14,6 @@
 # Setup some constants:
 # ---------------------
 
-USER_CREATE=marc # Set to user who creates the kernel file.
-
 SERIAL_DEVICE=/dev/ttyUSB0 # Serial device to communicate with Raspi.
 
 KERNEL_FILENAME=kernel.img # Name of kernel file to send to Raspi.
@@ -28,7 +26,7 @@ echo "*** Running creation script.. ***"
 echo "*********************************"
 echo
 #
-su -c ./create.sh ${USER_CREATE}
+./create.sh
 #
 echo
 
@@ -37,14 +35,14 @@ echo "*** Configuring serial device.. ***"
 echo "***********************************"
 echo
 #
-stty -F ${SERIAL_DEVICE} 115200
+sudo bash -c "stty -F ${SERIAL_DEVICE} 115200"
 
 echo "******************************"
 echo "*** Sending created file.. ***"
 echo "******************************"
 echo
 #
-sx ${KERNEL_FILENAME} < ${SERIAL_DEVICE} > ${SERIAL_DEVICE}
+sudo bash -c "sx ${KERNEL_FILENAME} < ${SERIAL_DEVICE} > ${SERIAL_DEVICE}"
 #
 echo
 
@@ -53,4 +51,4 @@ echo "*** Sending start command.. ***"
 echo "*******************************"
 echo
 #
-echo -n g > ${SERIAL_DEVICE}
+sudo bash -c "echo -n g > ${SERIAL_DEVICE}"
