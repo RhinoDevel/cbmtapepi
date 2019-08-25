5b57c1317557ae6c2335c95d21779d870a6bc492
Marc <RhinoDevel@users.noreply.github.com>
2019-08-25 17:45:51 +0200

    Linux OS support: Initializations implemented for tape transfer!

--- linux/main.c
+++ linux/main.c
@@ -4,14 +4,37 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 #include "../hardware/peribase.h"
 #include "../lib/mem/mem.h"
+#include "../lib/alloc/alloc.h"
 #include "../lib/console/console_params.h"
 #include "../lib/console/console.h"
 #include "../hardware/baregpio/baregpio.h"
 #include "../hardware/baregpio/baregpio_params.h"
 #include "../app/config.h"
+#include "../app/tape/tape_init.h"
+
+static void timer_start_one_mhz()
+{
+    // Nothing to do.
+}
+
+static uint32_t timer_get_tick()
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, 0); // (return value assumed to be zero)
+
+    return 1000000 * tv.tv_sec + tv.tv_usec;
+}
+
+static void timer_wait_microseconds(uint32_t const microseconds)
+{
+    usleep(microseconds); // (implicit cast & return value ignored)
+}
 
 /**
  * - Must not be called. Just for error handling..
@@ -28,17 +51,12 @@ static void write_byte(uint8_t const byte)
     putchar((int)byte);
 }
 
-static void wait_microseconds(uint32_t const microseconds)
-{
-    usleep(microseconds); // (implicit cast & return value ignored)
-}
-
 static void init_gpio()
 {
     // Initialize bare GPIO singleton:
     //
     baregpio_init((struct baregpio_params){
-        .wait_microseconds = wait_microseconds,
+        .wait_microseconds = timer_wait_microseconds,
 
         .mem_read = mem_read,
         .mem_write = mem_write,
@@ -67,9 +85,19 @@ static void init_console()
 
 int main()
 {
+    uint8_t * const mem = malloc(MT_HEAP_SIZE * sizeof *mem);
+
     init_gpio();
 
     init_console();
+
+    // Initialize memory (heap) manager for dynamic allocation/deallocation:
+    //
+    alloc_init((void*)mem, MT_HEAP_SIZE);
+
+    // Initialize for tape transfer:
+    //
+    tape_init(timer_start_one_mhz, timer_get_tick, timer_wait_microseconds);
 
     // Console test:
     //
@@ -87,5 +115,6 @@ int main()
     //
     // baregpio_set_output(MT_GPIO_PIN_NR_LED, false);
 
+    free(mem);
     return 0;
 }
