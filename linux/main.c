10f3bbb6dbf690e4659b41a55ab3c920fcfba515
Marc <RhinoDevel@users.noreply.github.com>
2019-08-26 11:30:12 +0200

    Untested sending in Linux OS mode.

--- linux/main.c
+++ linux/main.c
@@ -6,8 +6,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <sys/time.h>
+//#include <dirent.h>
+#include <sys/stat.h>
 
 #include "../hardware/peribase.h"
+#include "../lib/str/str.h"
 #include "../lib/mem/mem.h"
 #include "../lib/alloc/alloc.h"
 #include "../lib/console/console_params.h"
@@ -16,6 +19,9 @@
 #include "../hardware/baregpio/baregpio_params.h"
 #include "../app/config.h"
 #include "../app/tape/tape_init.h"
+#include "../app/tape/tape_send_params.h"
+#include "../app/tape/tape_input.h"
+#include "../app/tape/tape_send.h"
 
 static uint8_t * s_mem = NULL; // [see init() and deinit()]
 
@@ -113,11 +119,163 @@ static void init()
     tape_init(timer_start_one_mhz, timer_get_tick, timer_wait_microseconds);
 }
 
+static void fill_name(uint8_t * const name_out, char const * const name_in)
+{
+    // static uint8_t const sample_name[] = {
+    //     'R', 'H', 'I', 'N', 'O', 'D', 'E', 'V', 'E', 'L',
+    //     0x20, 0x20, 0x20, 0x20, 0x20, 0x20
+    // };
+
+    int i = 0;
+    char * const buf = alloc_alloc(str_get_len(name_in) + 1);
+
+    str_to_upper(buf, name_in);
+
+    while(i < MT_TAPE_INPUT_NAME_LEN && buf[i] != '\0')
+    {
+        name_out[i] = (uint8_t)buf[i]; // TODO: Implement real conversion to PETSCII.
+        ++i;
+    }
+    while(i < MT_TAPE_INPUT_NAME_LEN)
+    {
+        name_out[i] = 0x20;
+        ++i;
+    }
+
+    alloc_free(buf);
+}
+
+static void fill_add_bytes(uint8_t * const add_bytes)
+{
+    // Additional bytes (to be stored in header):
+    //
+    for(int i = 0;i<MT_TAPE_INPUT_ADD_BYTES_LEN;++i)
+    {
+        add_bytes[i] = 0x20;
+    }
+}
+
+static bool send_to_commodore(
+    uint8_t /*const*/ * const bytes,
+    char const * const name,
+    uint32_t const count)
+{
+    bool ret_val = false;
+    struct tape_send_params p;
+    uint32_t * const mem_addr = alloc_alloc(4 * 1024 * 1024); // Hard-coded
+
+    p.is_stop_requested = NULL;
+    p.gpio_pin_nr_read = MT_TAPE_GPIO_PIN_NR_READ;
+    p.gpio_pin_nr_sense = MT_TAPE_GPIO_PIN_NR_SENSE;
+    p.gpio_pin_nr_motor = MT_TAPE_GPIO_PIN_NR_MOTOR;
+    p.data = alloc_alloc(sizeof *(p.data));
+
+    fill_name(p.data->name, name);
+
+    p.data->type = tape_filetype_relocatable; // (necessary for PET PRG file)
+    //
+    // Hard-coded - maybe not always correct, but works for C64 and PET,
+    // both with machine language and BASIC PRG files.
+
+    // First two bytes hold the start address:
+    //
+    p.data->addr = *((uint16_t const *)bytes);
+    p.data->bytes = bytes + 2;
+    p.data->len = count - 2;
+
+    fill_add_bytes(p.data->add_bytes);
+
+#ifndef NDEBUG
+    console_write("Start address is 0x");
+    console_write_word(p.data->addr);
+    console_write(" (");
+    console_write_word_dec(p.data->addr);
+    console_writeline(").");
+#endif //NDEBUG
+
+    ret_val = tape_send(&p, mem_addr);
+
+    alloc_free(mem_addr);
+    alloc_free(p.data);
+    return ret_val;
+}
+
+/** Source: http://stackoverflow.com/questions/8236/how-do-you-determine-the-size-of-a-file-in-c
+ */
+off_t get_file_size(char const * const path)
+{
+    struct stat s;
+
+    //assert(path != NULL);
+
+    if(stat(path, &s) == 0)
+    {
+        return s.st_size;
+    }
+    return -1;
+}
+
+/** Return content of file at given path.
+ *
+ *  - Returns NULL on error.
+ *  - Caller takes ownership of return value.
+ */
+static unsigned char * load_file(char const * const path, off_t * const out_size)
+{
+    *out_size = -1;
+
+    off_t const signed_size = get_file_size(path);
+
+    if(signed_size == -1)
+    {
+        console_writeline("Error: Failed to get size of file!");
+        return NULL;
+    }
+
+    FILE * const file = fopen(path, "rb");
+
+    if(file == NULL)
+    {
+        console_writeline("Error: Failed to open source file!");
+        return NULL;
+    }
+
+    size_t const size = (size_t)signed_size;
+    unsigned char * const buf = malloc(size * sizeof *buf);
+
+    if(fread(buf, sizeof(*buf), size, file) != size)
+    {
+        console_writeline("Error: Failed to completely load file content!");
+        return NULL;
+    }
+
+    fclose(file);
+    *out_size = signed_size;
+    return buf;
+}
+
 static bool send(char * const file_name)
 {
-    console_writeline("Send mode is not implemented, yet.");
+    // Assuming uint8_t being equal to unsigned char.
 
-    return false;
+    off_t size = 0;
+    uint8_t /*const*/ * const bytes = load_file(file_name, &size);
+
+    if(bytes == NULL)
+    {
+        return false;
+    }
+
+    char * const name = file_name; // TODO: Remove eventually trailing path!
+    uint32_t const count = (uint32_t)size;
+
+    if(!send_to_commodore(bytes, name, count))
+    {
+        free(bytes);
+        return false;
+    }
+    free(bytes);
+    return true;
 }
 
 static bool receive()
