b340e42ad0471fde5ebb3b4bf4204e06f8495eb5
Marc <RhinoDevel@users.noreply.github.com>
2019-09-04 18:01:03 +0200

    mailbox_read_clockrate().

--- hardware/mailbox/mailbox.c
+++ hardware/mailbox/mailbox.c
@@ -20,8 +20,12 @@ static uint32_t const s_mailbox0_write = s_mailbox0_base + 0x20;
 //
 // Upper 28 bits hold value, lower 4 bits hold the receiving channel's number.
 
+static uint32_t const s_id_tag_getclockrate = 0x00030002;
+
 static uint32_t const s_status_empty = 0x40000000; // (bit 30 set, if empty)
 static uint32_t const s_status_full = 0x80000000; // (bit 31 set, if full)
+
+static uint32_t const s_responsecode_success = 0x80000000;
 
 void mailbox_write(uint32_t const channel, uint32_t const val)
 {
@@ -51,9 +55,74 @@ uint32_t mailbox_read(uint32_t const channel)
         val = mem_read(s_mailbox0_read);
         if((val & 0xF) == channel) // Lower 4 bits.
         {
-            return val >> 4; // Higher 28 bits.
+            return val >> 4; // Return higher 28 bits.
         }
     }
 
     //assert(false);
+}
+
+/**
+ * - Returns UINT32_MAX on error.
+ */
+uint32_t mailbox_read_clockrate(enum mailbox_id_clockrate const id)
+{
+    static uint32_t const channel_nr = 8; // 8 = Property channel nr.
+
+    volatile uint32_t msg_buf[8] __attribute__((aligned (16)));
+    //
+    // "__attribute__", etc. seems to be GCC-specific..
+
+	uint32_t i = 1;
+
+    // (first array element will be filled, below)
+    msg_buf[i++] = 0; // This is a request.
+
+    // Tag starts here:
+
+	msg_buf[i++] = s_id_tag_getclockrate; // Tag identity.
+	msg_buf[i++] = 2 * sizeof *msg_buf; // Size of value buffer in byte.
+    msg_buf[i++] = 0; // Tag's request code (sending => zero).
+
+    // (uint8_t) value buffer starts here:
+
+    msg_buf[i++] = (uint32_t)id; // Parameter
+	msg_buf[i++] = 0; // Used for response, too.
+
+    // (uint8_t) value buffer ended here.
+
+    // Tag ended here.
+
+    msg_buf[i++] = 0; // The end tag.
+
+    msg_buf[0] = sizeof *msg_buf * i; // Total size of buffer in byte (4 * 8).
+
+    mailbox_write(
+        channel_nr,
+        (0x40000000 + (uint32_t)msg_buf) // TODO: Replace hard-coded conversion to physical memory address. Assuming L2 cache to be enabled, otherwise 0xC0000000 would be correct!
+            >> 4);
+
+	mailbox_read(channel_nr); // (return value ignored)
+
+// #ifndef NDEBUG
+//     for(i = 0; i < sizeof msg_buf / sizeof *msg_buf; ++i)
+//     {
+//         console_write("mailbox_read_clockrate: Read at index ");
+//         console_write_dword_dec(i);
+//         console_write(": 0x");
+//         console_write_dword(msg_buf[i]);
+//         console_writeline(".");
+//     }
+// #endif //NDEBUG
+
+	if (msg_buf[1] == s_responsecode_success)
+    {
+        if(msg_buf[4] == (s_responsecode_success | 8)) // Expecting 8 bytes.
+        {
+		    return msg_buf[6];
+            //
+            // (works here, but value buffer is a uint8_t array)
+        }
+	}
+	return UINT32_MAX;
 }
--- hardware/mailbox/mailbox.h
+++ hardware/mailbox/mailbox.h
@@ -6,6 +6,20 @@
 
 #include <stdint.h>
 
+enum mailbox_id_clockrate
+{
+    mailbox_id_clockrate_emmc = 1,
+    mailbox_id_clockrate_uart = 2,
+    mailbox_id_clockrate_arm = 3,
+    mailbox_id_clockrate_core = 4,
+    mailbox_id_clockrate_v3d = 5,
+    mailbox_id_clockrate_h264 = 6,
+    mailbox_id_clockrate_isp = 7,
+    mailbox_id_clockrate_sdram = 8,
+    mailbox_id_clockrate_pixel = 9,
+    mailbox_id_clockrate_pwm = 10
+};
+
 /**
  * - Expects 28 bit value.
  */
@@ -15,5 +29,7 @@ void mailbox_write(uint32_t const channel, uint32_t const val);
  * - Returns higher 28 bits (without the channel nr.).
  */
 uint32_t mailbox_read(uint32_t const channel);
+
+uint32_t mailbox_read_clockrate(enum mailbox_id_clockrate const id);
 
 #endif //MT_MAILBOX
