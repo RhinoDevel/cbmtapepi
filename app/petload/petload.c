edba8aef273e9dc37ece2d1b7b5fb2a06e7e93da
Marc <RhinoDevel@users.noreply.github.com>
2019-12-17 22:02:25 +0100

    Improve comments; Add sample data ("loader").

--- app/petload/petload.c
+++ app/petload/petload.c
@@ -10,15 +10,18 @@
 #include <stdint.h>
 
 static char const * const s_name = "petload";
-static uint16_t const s_addr_tape_buf_one = 634/*0x27A*/; // v2 ROM.
+
+static uint16_t const s_addr_tape_buf_one = 634/*0x27A*/;
+//
+// PET ROM v2 and v3 tape #1 buffer.
+
 static uint16_t const s_addr_offset = 5 + MT_TAPE_INPUT_NAME_LEN; // Magic.
-    //
-    // TODO: Correct, or 5 + actual name's length?
 
 struct tape_input * petload_create()
 {
     struct tape_input * const ret_val = alloc_alloc(sizeof *ret_val);
     uint32_t len_buf = 0;
+    int i = 0;
 
     cbm_send_fill_name(ret_val->name, s_name);
     ret_val->type = tape_filetype_relocatable; // Correct for PET.
@@ -29,11 +32,20 @@ struct tape_input * petload_create()
         true,
         &len_buf);
 
-    assert(len_buf > 0 && len_buf <= 0xFFFF);
+    assert(len_buf > 0 && len_buf <= 0x0000FFFF);
 
     ret_val->len = (uint16_t)len_buf;
 
-    // TODO: Fill ret_val->add_bytes with actual loader (behind name)!
+    ret_val->add_bytes[i++] = 169; // Immediate LDA.
+    ret_val->add_bytes[i++] = 83; // Heart symbol (yes, it is romantic).
+    ret_val->add_bytes[i++] = 141; // Absolute STA.
+    ret_val->add_bytes[i++] = 0; // Lower byte of 32768 (0x8000 - PET video RAM start).
+    ret_val->add_bytes[i++] = 128; // Higher byte of 32768.
+    ret_val->add_bytes[i++] = 96; // RTS.
+    while(i < MT_TAPE_INPUT_ADD_BYTES_LEN)
+    {
+        ret_val->add_bytes[i++] = 0;
+    }
 
     return ret_val;
 }
