bfe0e16dc85d3a347463287d298ddfb92a937023
Marc <RhinoDevel@users.noreply.github.com>
2020-12-19 21:23:28 +0100

    UNTESTED: filesys_load() and filesys_save().

--- lib/filesys/filesys.c
+++ lib/filesys/filesys.c
@@ -4,8 +4,16 @@
 #include "filesys.h"
 #include "../alloc/alloc.h"
 #include "../ff14/source/ff.h"
+#include "../assert.h"
+#include "../dir/dir.h"
+#include "../ff14/source/ff.h"
+
+#ifndef NDEBUG
+    #include "../../lib/console/console.h"
+#endif //NDEBUG
 
 #include <stdbool.h>
+#include <stdint.h>
 
 static FATFS * s_fs = 0;
 
@@ -62,4 +70,122 @@ bool filesys_remount()
         }
     }
     return mount();
+}
+
+uint8_t* filesys_load(
+    char const * const dir_path,
+    char const * const filename,
+    uint32_t * const out_byte_count)
+{
+    assert(out_byte_count != 0);
+
+    FIL fil;
+    UINT read_len = 0;
+
+    filesys_remount();
+    dir_reinit(dir_path);
+
+    char * const full_path = dir_create_full_path(dir_path, filename);
+
+    if(f_open(&fil, full_path, FA_READ) != FR_OK)
+    {
+#ifndef NDEBUG
+        console_write("filesys_load : Error: Opening file \"");
+        console_write(full_path);
+        console_writeline("\" failed!");
+#endif //NDEBUG
+        alloc_free(full_path);
+        dir_deinit();
+        filesys_unmount();
+        *out_byte_count = 0;
+        return 0;
+    }
+
+    uint32_t const count = (uint32_t)f_size(&fil);
+
+    uint8_t * const bytes = alloc_alloc(count * sizeof *bytes + 1);
+    //
+    // (+1 to support empty files)
+
+    f_read(&fil, bytes, (UINT)count, &read_len);
+
+#ifndef NDEBUG
+    assert(count == read_len);
+
+    console_write("filesys_load: Read ");
+    console_write_dword_dec(read_len);
+    console_writeline(" bytes.");
+#endif
+
+    f_close(&fil);
+    alloc_free(full_path);
+    dir_deinit();
+    filesys_unmount();
+
+    *out_byte_count = read_len;
+    return bytes;
+}
+
+bool filesys_save(
+    char const * const dir_path,
+    char const * const filename,
+    uint8_t const * const bytes,
+    uint32_t const byte_count,
+    bool const overwrite)
+{
+    bool ret_val = false,
+        file_is_open = false;
+    FIL fil;
+
+    filesys_remount();
+    dir_reinit(dir_path);
+
+    char * const full_path = dir_create_full_path(dir_path, filename);
+
+    do
+    {
+        UINT write_count;
+        BYTE mode = FA_WRITE;
+
+        if(overwrite)
+        {
+            mode |= FA_CREATE_ALWAYS;
+        }
+        else
+        {
+            mode |= FA_CREATE_NEW;
+        }
+
+        if(f_open(&fil, full_path, mode) != FR_OK)
+        {
+            break;
+        }
+        file_is_open = true;
+
+        if(f_write(&fil, bytes, byte_count, &write_count) != FR_OK)
+        {
+            break;
+        }
+        if(write_count != byte_count)
+        {
+            break;
+        }
+
+        ret_val = true;
+    }while(false);
+
+    if(file_is_open)
+    {
+        f_close(&fil);
+        file_is_open = false;
+
+        if(!ret_val)
+        {
+            f_unlink(filename);
+        }
+    }
+    alloc_free(full_path);
+    dir_deinit();
+    filesys_unmount();
+    return ret_val;
 }
--- lib/filesys/filesys.h
+++ lib/filesys/filesys.h
@@ -7,6 +7,7 @@
 #define MT_FILESYS
 
 #include <stdbool.h>
+#include <stdint.h>
 
 /**
  * - OK to be called, if already unmounted (returns true).
@@ -14,5 +15,29 @@
 bool filesys_unmount();
 
 bool filesys_remount();
+
+/** Load full file content and set byte count.
+ * 
+ *  - Supports empty files.
+ *  - Caller takes ownership of returned object.
+ *  - Returns 0 on error.
+ */
+uint8_t* filesys_load(
+    char const * const dir_path,
+    char const * const filename,
+    uint32_t * const out_byte_count);
+
+/** Saves given count of bytes starting at given pointer at given directory with
+ *  given file name.
+ * 
+ *  - Supports empty files.
+ *  - Returns, if successful or not.
+ */
+bool filesys_save(
+    char const * const dir_path,
+    char const * const filename,
+    uint8_t const * const bytes,
+    uint32_t const byte_count,
+    bool const overwrite);
 
 #endif //MT_FILESYS
