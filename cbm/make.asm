f7b6ce08b42465d06bb72f4037f04943475adecc
Marc <RhinoDevel@users.noreply.github.com>
2021-03-04 08:37:48 +0100

    Improve "makefile".

--- cbm/make.asm
+++ cbm/make.asm
@@ -11,7 +11,7 @@ Incasm "defines.asm"
 ; choose one file to either install into top of memory via basic loader,
 ; or to install into tape buffers via directly loading into tape buffers:
 ;
-Incasm "topofmem.asm" ; TODO: currently installs into tape buffers, too.
+;Incasm "topofmem.asm" ; TODO: currently installs into tape buffers, too.
 Incasm "tapebuf.asm"
 
 Incasm "install.asm"
@@ -20,3 +20,5 @@ Incasm "main.asm"
 Incasm "sendbyte.asm"
 Incasm "readbyte.asm"
 Incasm "deb.asm"
+
+Generateto cbmtapepi.prg
