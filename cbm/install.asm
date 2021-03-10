7d556ce3b8650eea46cfe01792d87ef22337199a
Marc <RhinoDevel@users.noreply.github.com>
2021-03-10 21:01:55 +0100

    No jump to NEW on TOM install; Comments.

--- cbm/install.asm
+++ cbm/install.asm
@@ -9,6 +9,9 @@
 ; *** wedge installer *** (space reused after installation for cmd. string)
 ; ***********************
 
+; 16 bytes (from str label on) are reused for cmd. string by wedge, but the
+; other bytes can be used for something else (currently wasted..)!
+;
 str      lda #$4c       ; jmp
          sta chrget
 install_low
@@ -17,17 +20,14 @@ install_low
 install_high
          lda #>wedge
          sta chrget + 2
-         ;jmp new
-         ;byte 0
-
-if bas_ver = 20
-         jsr new
-         byte 0,0,0,0,0,0,0,0 ; TODO: replace (above jump..)!
-else
-         lda cas_wrt    ; set write line level to low (7 bytes "wasted",
-         and #oudmaskn  ; because not used later by fast mode wedge).
-         sta cas_wrt    ;
          
+         lda cas_wrt    ; set write line level to low to signal pi.
+         and #oudmaskn  ; 
+         sta cas_wrt    ;
+
+if tom_install = 1
+         rts
+else
          jmp new ; repairs start-of-variables pointer, etc., these were changed
                  ; by loading into tape buffer(-s).
 endif
