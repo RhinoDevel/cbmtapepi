78c0cc99c8edc12d13ed6d13a05aca733fb9a8cc
Marc <RhinoDevel@users.noreply.github.com>
2021-03-21 23:33:21 +0100

    Move signal detection to ISR (works, but not "fast enough" - fix that!); Disable video output (again).

--- app/kernel_main.c
+++ app/kernel_main.c
@@ -17,6 +17,7 @@
 #include "../hardware/gpio/gpio_params.h"
 #include "../hardware/gpio/gpio.h"
 #ifndef NDEBUG
+//    #include "../hardware/systimer/systimer.h"
     #include "../hardware/sdcard/sdcard.h"
 #endif //NDEBUG
 
@@ -48,15 +49,14 @@
 #include "mode/mode.h"
 #include "mode/mode_type.h"
 
-#if PERI_BASE == PERI_BASE_PI1
-    #define VIDEO_SUPPORT 1
-#else //PERI_BASE == PERI_BASE_PI1
-    #define VIDEO_SUPPORT 0
-#endif //PERI_BASE == PERI_BASE_PI1
-
-#if VIDEO_SUPPORT
-    #include "../lib/video/video.h"
-#endif //VIDEO_SUPPORT
+// #if PERI_BASE == PERI_BASE_PI1
+//     #define VIDEO_SUPPORT 1
+// #else //PERI_BASE == PERI_BASE_PI1
+//     #define VIDEO_SUPPORT 0
+// #endif //PERI_BASE == PERI_BASE_PI1
+// #if VIDEO_SUPPORT
+//     #include "../lib/video/video.h"
+// #endif //VIDEO_SUPPORT
 
 extern uint32_t __heap; // See memmap.ld.
 
@@ -69,15 +69,37 @@ enum led_state
 
 // For fast mode installer ready-signal detection (it has a frequency):
 //
-static int const s_t_cycle = 500; // Microseconds <=> 2kHz fastmode freq.
-static int const s_per_cycle = 5; // Count of measure points per fastmode
-                                // installer signal cycle.
-static int const s_t_measure = s_t_cycle / s_per_cycle; // <=> 10kHz sampling.
-static int const s_cycles = 10; // Count of fastmode cycles for measuring.
-static int const s_measurements = s_cycles * s_per_cycle; // Count of samples.
-static int const s_min_toggles = 10; // Count of min. level changes.
+#define s_t_cycle 500 // Microseconds <=> 2kHz fastmode freq.
+#define s_per_cycle 5 // Count of measure points per fastmode
+                      // installer signal cycle.
+#define s_t_measure (s_t_cycle / s_per_cycle) // <=> 10kHz sampling.
+#define s_cycles 10 // Count of fastmode cycles for measuring.
+#define s_measurements (s_cycles * s_per_cycle) // Count of samples.
+#define s_min_toggles 10 // Count of min. level changes.
+//
+// Will be intialized in init_signal_stuff():
+//
+static bool s_levels[s_measurements];
+static int s_level_index = 0;
+
+/*
+ * - Set to false by init_signal_stuff() and send_petload_loop(),
+ *   set to true by IRQ handler.
+ */
+static bool s_signal_existed = false;
 
 static enum led_state s_led_state = led_state_off;
+
+static void init_signal_stuff()
+{
+    s_level_index = 0;
+    s_signal_existed = false;
+
+    for(int i = 0; i < s_measurements; ++i)
+    {
+        s_levels[i] = 0;
+    }
+}
 
 /** IRQ interrupt handler.
  */
@@ -119,6 +141,32 @@ void __attribute__((interrupt("IRQ"))) handler_irq()
         MT_GPIO_PIN_NR_LED, 
         s_led_state == led_state_on
             || (s_led_state != led_state_off && blink_state));
+
+    // *** SIGNAL DETECTION: ***
+
+    // If there are at least the minimum amount of level changes between the
+    // last measurements, set a flag.
+
+    // It is OK to call gpio_read() from ISR ("atomic" considerations)
+    // - see implementation of gpio_read()!
+
+    s_levels[s_level_index] = gpio_read(MT_TAPE_GPIO_PIN_NR_WRITE);
+    for(int i = 0, c = 0;i < s_measurements; ++i)
+    {
+        int const cur_index = 
+                    (s_level_index - i + s_measurements) % s_measurements,
+                pre_index = (cur_index + s_measurements - 1) % s_measurements;
+
+        c += (int)(s_levels[cur_index] != s_levels[pre_index]);
+        if(c == s_min_toggles)
+        {
+            s_signal_existed = true;
+            break;
+        }
+    }
+    s_level_index = (s_level_index + 1) % s_measurements;
+
+    // *************************
 
 //     // Use this via video output and do not output too much to influence
 //     // measurement results (second number output must equal the wanted interrupt
@@ -321,7 +369,9 @@ void __attribute__((interrupt("IRQ"))) handler_irq()
         }
     }
 
-    /*
+    // This is done by IRQ handler, now:
+    //
+/*
     [TAPE: Tmin = 352us, Tmax = 672us]
     WRITE: Trise < 2us, Tfall < 4us
     SCNKEY (at VIC 20 via ISR): T60Hz = 16667us, T50Hz = 20000us
@@ -348,47 +398,54 @@ void __attribute__((interrupt("IRQ"))) handler_irq()
             Tarm = T / 5  =                  100us (Tarm >> Trise and Tfall)
             Tx   = 10 * T = 10 * 5 * Tarm = 5000us
             x    =                            10
-
-    FIRST STEP: Implementation in loop, without ISR!
-    */
+*/
     ///
-    /** Tries to detect a 50% duty cycle, logic level changing signal on GPIO
-     *  port with given nr.
-     * 
-     * - Takes almost 5ms (depends on values below).
-     */
-    static bool is_signal_detected(uint32_t const gpio_pin_nr)
+//     /** Tries to detect a 50% duty cycle, logic level changing signal on GPIO
+//      *  port with given nr.
+//      * 
+//      * - Takes almost 5ms (depends on values below).
+//      */
+//     static bool is_signal_detected(uint32_t const gpio_pin_nr)
+//     {
+//         uint32_t toggles = 0; // Level changes counted in measurement timespan.
+//         bool last = gpio_read(gpio_pin_nr); // Store last read logic level.
+//
+//         for(int i = 1;i < s_measurements; ++i) // (first sample taken above)
+//         {
+//             armtimer_busywait_microseconds(s_t_measure);
+//
+//             if(gpio_read(gpio_pin_nr) != last)
+//             {
+//                 ++toggles;
+//                 last = !last;
+//             }
+//         }
+//       
+// #ifndef NDEBUG
+//         console_write("is_fastmode_installer_waiting : Counted ");
+//         console_write_dword_dec(toggles);
+//         console_writeline(" level changes.");
+// #endif //NDEBUG
+//
+//         return toggles >= s_min_toggles;
+//     }
+
+    static bool did_signal_exist()
     {
-        uint32_t toggles = 0; // Level changes counted in measurement timespan.
-        bool last = gpio_read(gpio_pin_nr); // Store last read logic level.
-
-        for(int i = 1;i < s_measurements; ++i) // (first sample taken above)
-        {
-            armtimer_busywait_microseconds(s_t_measure);
-
-            if(gpio_read(gpio_pin_nr) != last)
-            {
-                ++toggles;
-                last = !last;
-            }
-        }
-        
-#ifndef NDEBUG
-        console_write("is_fastmode_installer_waiting : Counted ");
-        console_write_dword_dec(toggles);
-        console_writeline(" level changes.");
-#endif //NDEBUG
-
-        return toggles >= s_min_toggles;
+        return s_signal_existed;
     }
 
-    static void send_petload(enum mode_type const mode)
+    /**
+     * - Returns false, if fastmode installer ran on CBM.
+     */
+    static bool send_petload(enum mode_type const mode)
     {
         assert(mode == mode_type_pet1 || mode == mode_type_pet1tom
             || mode == mode_type_pet2 || mode == mode_type_pet2tom
             || mode == mode_type_pet4 || mode == mode_type_pet4tom
             || mode == mode_type_vic20tom);
 
+        bool ret_val = true;
         struct tape_input * ti = 0;
     
         switch(mode)
@@ -441,10 +498,24 @@ void __attribute__((interrupt("IRQ"))) handler_irq()
 
         s_led_state = led_state_off; // Indicates LOAD mode (IRQ).
     
-        cbm_send_data(ti, 0);
+        // Kind of bad: False is never interpreted as error, here.
+        //              It is assumed that the only reason for this function to
+        //              return false is that the fastmode installer signal got
+        //              retrieved by the interrupt handler:
+        //
+        ret_val = cbm_send_data(ti, did_signal_exist);
     
         tape_input_free(ti);
         s_led_state = led_state_on; // Indicates SAVE mode (IRQ).
+
+        // TODO: Does not seem to work?!
+        //
+        // Wait for signal from installer:
+        //
+        armtimer_busywait_microseconds(100 * 1000); // 100ms.
+        ret_val = !did_signal_exist();
+
+        return ret_val;
     }
     static void send_petload_loop(enum mode_type const mode)
     {
@@ -452,22 +523,11 @@ void __attribute__((interrupt("IRQ"))) handler_irq()
         //  because VIA's port used for tape WRITE is used for keyboard scan,
         //  too..)
 
+        s_signal_existed = false;
+
         while(true)
         {
-            send_petload(mode);
-
-            // TODO: Probably unnecessary:
-            //
-            // Wait to let CBM fastmode installer enough time to detect that
-            // SENSE got set to HIGH at CBM, which triggers infinite sending of
-            // signal we want to detect: 
-            //
-            armtimer_busywait_microseconds(10);
-
-            // TODO: Does not work with using <shift> + <run/stop>
-            //       (sense detection??):
-            //
-            if(is_signal_detected(MT_TAPE_GPIO_PIN_NR_WRITE))
+            if(!send_petload(mode))
             {
                 console_deb_writeline("send_petload_loop : Breaking loop..");
                 return;
@@ -607,13 +667,13 @@ void __attribute__((interrupt("IRQ"))) handler_irq()
     }
 #endif //MT_INTERACTIVE
 
-#if VIDEO_SUPPORT
-#else //VIDEO_SUPPORT
-    static void dummy_write(uint8_t const byte)
-    {
-        (void)byte; // Doing nothing.
-    }
-#endif //VIDEO_SUPPORT
+// #if VIDEO_SUPPORT
+// #else //VIDEO_SUPPORT
+//     static void dummy_write(uint8_t const byte)
+//     {
+//         (void)byte; // Doing nothing.
+//     }
+// #endif //VIDEO_SUPPORT
 
 /** Connect console (singleton) to wanted in-/output.
  */
@@ -637,14 +697,14 @@ static void init_console()
     // may cause serial transfers to fail (e.g. assertions and debug output)!
     //
     p.write_newline_with_cr = true;
-    //
-#if VIDEO_SUPPORT
-    p.write_byte = video_write_byte;
-#else //VIDEO_SUPPORT
-    p.write_byte = dummy_write;
-#endif //VIDEO_SUPPORT
-    //
-    p.write_newline_with_cr = false;
+    
+// #if VIDEO_SUPPORT
+//     p.write_byte = video_write_byte;
+// #else //VIDEO_SUPPORT
+//     p.write_byte = dummy_write;
+// #endif //VIDEO_SUPPORT
+    
+ //   p.write_newline_with_cr = false;
 
     miniuart_init();
 
@@ -705,13 +765,13 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t r2)
     //
     init_console();
 
-    // Does not work for Raspberry Pi 2, yet:
-    //
-    // Initialize video:
-    //
-#if VIDEO_SUPPORT
-    video_init();
-#endif //VIDEO_SUPPORT
+//     // Does not work for Raspberry Pi 2, yet:
+//     //
+//     // Initialize video:
+//     //
+// #if VIDEO_SUPPORT
+//     video_init();
+// #endif //VIDEO_SUPPORT
 
 #ifndef NDEBUG
     int const result_sdcard_init = sdcard_init();
@@ -736,7 +796,11 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t r2)
         armtimer_get_tick,
         armtimer_busywait_microseconds);
 
+    init_signal_stuff();
+
     irq_armtimer_init(); // Needs GPIO, initializes LED GPIOs, too.
+                         // Also needs tape_init() and init_signal_stuff()
+                         // to already have been called.
 
 #ifdef MT_INTERACTIVE
     // Start user interface (via console):
