9757cf6171a3dc12d92ca7cf9d91eeeeb7ffa0ee
Marc <RhinoDevel@users.noreply.github.com>
2021-05-27 22:06:43 +0200

    More C++ compatibility.

--- lib/petasc/petasc.h
+++ lib/petasc/petasc.h
@@ -8,6 +8,10 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * - Upper case ASCII letters given are interpreted as lower case.
  * - No support for control codes of any kind (CR, LF, etc.).
@@ -19,5 +23,9 @@ char petasc_get_petscii(char const c, char const not_found_replacer);
  * - No support for control codes of any kind (CR, LF, etc.).
  */
 char petasc_get_ascii(char const c, char const not_found_replacer);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //MT_PETASC
