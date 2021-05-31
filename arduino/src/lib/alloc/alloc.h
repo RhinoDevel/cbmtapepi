37f778e4e9f0b0aca40e9f66ddc3833f4c37244b
Marc <RhinoDevel@users.noreply.github.com>
2021-05-31 22:48:03 +0200

    Use malloc() and free() instead of alloc_alloc() and alloc_free().

---
+++ arduino/src/lib/alloc/alloc.h
@@ -0,0 +1,12 @@
+
+// RhinoDevel, MT, 2021may31
+
+#ifndef MT_ALLOC
+#define MT_ALLOC
+
+#include <stdlib.h>
+
+#define alloc_free free
+#define alloc_alloc malloc
+
+#endif //MT_ALLOC
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -44,6 +44,7 @@
 #include "ESP8266_ISR_Timer.h"
 
 //#include "lib/assert.h"
+//#include "lib/alloc/alloc.h"
 #include "lib/console/console_params.h"
 #include "lib/console/console.h"
 
