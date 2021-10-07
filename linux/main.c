0b2b64111ed005d67faa2e99f92936f45a96c1d7
RhinoDevel <RhinoDevel@users.noreply.github.com>
2021-10-07 20:25:07 +0200

    Simplify code.

--- linux/main.c
+++ linux/main.c
@@ -11,12 +11,10 @@
 #include <pigpio.h>
 
 #include "../lib/str/str.h"
-#include "../lib/mem/mem.h"
 #include "../lib/alloc/alloc.h"
 #include "../lib/console/console_params.h"
 #include "../lib/console/console.h"
 #include "../app/config.h"
-#include "../app/tape/tape_send_params.h"
 #include "../app/tape/tape_input.h"
 #include "../app/tape/tape_fill_buf.h"
 #include "pigpio/pigpio.h"
@@ -35,6 +33,65 @@ static uint8_t dummy_read()
 static void write_byte(uint8_t const byte)
 {
     putchar((int)byte);
+}
+
+off_t get_file_size(char const * const path)
+{
+    assert(path != NULL);
+
+    struct stat s;
+
+    if(stat(path, &s) == 0)
+    {
+        return s.st_size;
+    }
+    return -1;
+}
+
+/** Return content of file at given path.
+ *
+ *  - Returns NULL on error.
+ *  - Caller takes ownership of return value.
+ */
+static unsigned char * load_file(
+    char const * const path, off_t * const out_size)
+{
+    *out_size = -1;
+
+    off_t const signed_size = get_file_size(path);
+
+    if(signed_size == -1)
+    {
+        console_writeline("load_file : Error: Failed to get size of file!");
+        return NULL;
+    }
+    if(signed_size > (off_t)s_max_file_size)
+    {
+        console_writeline("load_file : Error: File is too big!");
+        return NULL;
+    }
+
+    FILE * const file = fopen(path, "rb");
+
+    if(file == NULL)
+    {
+        console_writeline("load_file : Error: Failed to open source file!");
+        return NULL;
+    }
+
+    size_t const size = (size_t)signed_size;
+    unsigned char * const buf = malloc(size * sizeof *buf);
+
+    if(fread(buf, sizeof(*buf), size, file) != size)
+    {
+        console_writeline(
+            "load_file : Error: Failed to completely load file content!");
+        return NULL;
+    }
+
+    fclose(file);
+    *out_size = signed_size;
+    return buf;
 }
 
 /** Connect console (singleton) to wanted in-/output.
@@ -129,40 +186,35 @@ static void fill_name(uint8_t * const name_out, char const * const name_in)
     alloc_free(buf);
 }
 
-static struct tape_send_params * create_send_params(
-    uint8_t /*const*/ * const bytes,
-    char const * const name,
-    uint32_t const count)
+/**
+ * - Caller takes ownership of returned object.
+ */
+static struct tape_input * create_tape_input(
+    uint8_t * const bytes, uint32_t const count, char const * const name)
 {
-    struct tape_send_params * const ret_val = alloc_alloc(sizeof *ret_val);
+    struct tape_input * const ret_val = alloc_alloc(sizeof *ret_val);
 
-    ret_val->is_stop_requested = NULL;
-    ret_val->gpio_pin_nr_read = MT_TAPE_GPIO_PIN_NR_READ;
-    ret_val->gpio_pin_nr_sense = MT_TAPE_GPIO_PIN_NR_SENSE;
-    ret_val->gpio_pin_nr_motor = MT_TAPE_GPIO_PIN_NR_MOTOR;
-    ret_val->data = alloc_alloc(sizeof *(ret_val->data));
-
-    fill_name(ret_val->data->name, name);
+    fill_name(ret_val->name, name);
 
     // Hard-coded for PET PRG files. C64 (and other) machines need to load
     // PRGs that are not starting at BASIC start address / are not relocatable
     // as non-relocatable because of this (e.g.: LOAD"",1,1 on C64):
     //
-    ret_val->data->type = tape_filetype_relocatable;
+    ret_val->type = tape_filetype_relocatable;
 
     // First two bytes hold the start address:
     //
-    ret_val->data->addr = (((uint16_t)bytes[1]) << 8) | (uint16_t)bytes[0];
-    ret_val->data->bytes = bytes + 2;
-    ret_val->data->len = count - 2;
+    ret_val->addr = (((uint16_t)bytes[1]) << 8) | (uint16_t)bytes[0];
+    ret_val->bytes = bytes + 2;
+    ret_val->len = count - 2;
 
-    tape_input_fill_add_bytes(ret_val->data->add_bytes);
+    tape_input_fill_add_bytes(ret_val->add_bytes);
 
 #ifndef NDEBUG
-    console_write("Start address is 0x");
-    console_write_word(ret_val->data->addr);
+    console_write("create_tape_input: Start address is 0x");
+    console_write_word(ret_val->addr);
     console_write(" (");
-    console_write_word_dec(ret_val->data->addr);
+    console_write_word_dec(ret_val->addr);
     console_writeline(").");
 #endif //NDEBUG
 
@@ -170,74 +222,47 @@ static struct tape_send_params * create_send_params(
 }
 
 /**
- *  - Takes ownership of given object by freeing all of its memory.
+ * - Caller takes ownership of returned object.
  */
-static void free_send_params(struct tape_send_params * const p)
+static uint8_t* create_symbols_from_bytes(
+    uint8_t * const bytes,
+    uint32_t const byte_count,
+    char const * const name,
+    uint32_t * const out_symbol_count)
 {
-    alloc_free(p->data);
-    alloc_free(p);
+    struct tape_input * const t = create_tape_input(bytes, byte_count, name);
+    uint32_t * const ret_val = alloc_alloc(s_mem_buf_size);
+
+    *out_symbol_count = tape_fill_buf(t, (uint8_t * const)ret_val);
+
+    alloc_free(t);
+    return (uint8_t*)ret_val;
 }
 
-/** Source:
- *
- *  http://stackoverflow.com/questions/8236/how-do-you-determine-the-size-of-a-file-in-c
+/**
+ * - Caller takes ownership of returned object.
  */
-off_t get_file_size(char const * const path)
+static uint8_t* create_symbols_from_file(
+    char * const file_name, uint32_t * const out_symbol_count)
 {
-    assert(path != NULL);
+    // Assuming uint8_t being equal to unsigned char.
 
-    struct stat s;
+    off_t size = 0;
+    uint8_t * const bytes = load_file(file_name, &size);
 
-    if(stat(path, &s) == 0)
+    if(bytes == NULL)
     {
-        return s.st_size;
-    }
-    return -1;
-}
-
-/** Return content of file at given path.
- *
- *  - Returns NULL on error.
- *  - Caller takes ownership of return value.
- */
-static unsigned char * load_file(
-    char const * const path, off_t * const out_size)
-{
-    *out_size = -1;
-
-    off_t const signed_size = get_file_size(path);
-
-    if(signed_size == -1)
-    {
-        console_writeline("Error: Failed to get size of file!");
-        return NULL;
-    }
-    if(signed_size > (off_t)s_max_file_size)
-    {
-        console_writeline("Error: File is too big!");
-        return NULL;
+        return false;
     }
 
-    FILE * const file = fopen(path, "rb");
+    uint8_t * const symbols = create_symbols_from_bytes(
+        bytes,
+        (uint32_t)size,
+        file_name, // TODO: Remove maybe preceding path!
+        out_symbol_count);
 
-    if(file == NULL)
-    {
-        console_writeline("Error: Failed to open source file!");
-        return NULL;
-    }
-
-    size_t const size = (size_t)signed_size;
-    unsigned char * const buf = malloc(size * sizeof *buf);
-
-    if(fread(buf, sizeof(*buf), size, file) != size)
-    {
-        console_writeline("Error: Failed to completely load file content!");
-        return NULL;
-    }
-
-    fclose(file);
-    *out_size = signed_size;
-    return buf;
+    free(bytes);
+    return symbols;
 }
 
 static bool send(char * const file_name)
@@ -249,40 +274,22 @@ static bool send(char * const file_name)
 
 static bool symbols(char * const file_name)
 {
-    // Assuming uint8_t being equal to unsigned char.
-
-    off_t size = 0;
-    uint8_t /*const*/ * const bytes = load_file(file_name, &size);
-
-    if(bytes == NULL)
-    {
-        return false;
-    }
-
-    struct tape_send_params * const p = create_send_params(
-        bytes,
-        "dummy", // Name
-        (uint32_t)size);
-    uint32_t * const mem_addr = alloc_alloc(s_mem_buf_size);
-
-    int const symbol_count = tape_fill_buf(p->data, (uint8_t * const)mem_addr);
+    uint32_t symbol_count = 0;
+    uint8_t * const symbols = create_symbols_from_file(
+                                file_name, &symbol_count);
 
     for(int i = 0;i < symbol_count;++i)
     {
-        write_byte(((uint8_t * const)mem_addr)[i]);
+        write_byte(symbols[i]);
     }
 
     // TODO: Debugging:
     //
     assert(
-        pigpio_create_wave(
-            MT_TAPE_GPIO_PIN_NR_READ,
-            (uint8_t const * const)mem_addr,
-            symbol_count) >= 0);
+        pigpio_create_wave(MT_TAPE_GPIO_PIN_NR_READ, symbols, symbol_count)
+            >= 0);
 
-    alloc_free(mem_addr);
-    free_send_params(p);
-    free(bytes);
+    alloc_free(symbols);
     return true;
 }
 
