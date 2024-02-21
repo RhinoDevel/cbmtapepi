84b8766154a08edec1837845c2ef69875729c9c9
Marc <RhinoDevel@users.noreply.github.com>
2024-02-21 17:34:37 +0100

    Add UNTESTED Pi 4/400 peripheral base address option.

--- hardware/peribase.h
+++ hardware/peribase.h
@@ -16,6 +16,7 @@
 #define PERI_BASE_PI_VER 1
 //#define PERI_BASE_PI_VER 2
 //#define PERI_BASE_PI_VER 3
+//#define PERI_BASE_PI_VER 4
 
 // Peripherals physical address range start
 // (these replace bus address range start 0x7E000000 in bare metal mode,
@@ -23,6 +24,7 @@
 //
 #define PERI_BASE_PI0AND1 0x20000000 // BCM2835
 #define PERI_BASE_PI2AND3 0x3F000000 // BCM2836 and BCM2837.
+#define PERI_BASE_PI4AND400 0xFE000000 // BCM2711
 
 #if PERI_BASE_PI_VER == 0
     #define PERI_BASE PERI_BASE_PI0AND1
@@ -36,6 +38,9 @@
 #elif PERI_BASE_PI_VER == 3
     #define PERI_BASE PERI_BASE_PI2AND3
     //#define GPIO_PIN_NR_ACT // Not available (this way)!
-#endif //PERI_BASE_PI_VER == 3
+#elif PERI_BASE_PI_VER == 4
+    #define PERI_BASE PERI_BASE_PI4AND400 // TODO: Test this in bare metal mode!
+    //#define GPIO_PIN_NR_ACT // TODO: Not available (this way)?
+#endif //PERI_BASE_PI_VER == 4
 
 #endif //MT_PERIBASE
